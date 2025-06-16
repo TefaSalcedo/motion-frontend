6116b5f1bbe00ae98b694647d803557bfdc72f32
Tefa <stefa.21@hotmail.com>
2025-06-16 10:37:18 -0500

    creación de componentes para la pagina de home #5

---
+++ src/components/home/logo/motionlogo.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Imagologo from "../../../assets/Imagologo_motion.svg"
+
+const MotionLogo = () => {
+  return (
+    <>
+      <img
+        src={Imagologo} 
+        alt="Motion Innovation Logo"
+      />
+    </>
+  );
+}
+
+export default MotionLogo;

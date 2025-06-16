d5aeb6fadc71ab35673e66463c188eb93cfa7ffe
Tefa <stefa.21@hotmail.com>
2025-06-16 09:57:59 -0500

    Organización de assets y creación de componente inicial de Home #4

---
+++ src/components/home/monitoring/monitoring.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import telefonoImg from "../../../assets/Telefono-01.png"
+
+const Monitoring = () => {
+  return (
+    <div>
+      <h1>Bienvenido</h1>
+      <h2>Monitoring Innovation</h2>
+      <img
+        src={telefonoImg} 
+        alt="Motion Innovation Logo"
+      />
+    </div>
+  );
+}
+
+export default Monitoring;

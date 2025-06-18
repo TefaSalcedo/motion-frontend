3420ef6c4b691425d185fee4c1728aa94eacb19e
Tefa <stefa.21@hotmail.com>
2025-06-18 11:25:29 -0500

    cleaner code for imports unused #23

--- src/components/crud/crud-form/FormIcon.jsx
+++ src/components/crud/crud-form/FormIcon.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import IconCreate from "../../../assets/Icon_crear.svg";
 import { useStore } from "../store.jsx";
 import "./FormIcon.css"
